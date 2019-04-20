c4b6546653b8fa21e5f35adafd7eba5c7e55d787
Sean R Brown <sean-b@outlook.com>
2019-04-20 12:14:21 -0500

    Refactored the hashing into an abstract class.

---
+++ np-website/Models/Member.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace np_website.Models
+{
+    public abstract class Member
+    {
+        private const int SaltSize = 32;
+
+        /// <summary>
+        ///     Generates a random hash salt.
+        /// </summary>
+        /// 
+        internal static byte[] GenerateSalt()
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[SaltSize];
+
+                rng.GetBytes(randomNumber);
+
+                return randomNumber;
+
+            }
+        }
+
+        /// <summary>
+        ///     Hashes the input based on the given salt.
+        /// </summary>
+        /// 
+        /// <returns>
+        ///     Returns the hashed value.
+        /// </returns>
+        /// 
+        /// <param name="value">The value to hash</param>
+        /// <param name="salt">The salt used in the hashing algorithm.</param>
+        /// 
+        internal static byte[] ComputeHMAC_SHA256(string value, byte[] salt)
+        {
+            using (var hmac = new HMACSHA256(salt))
+            {
+                return hmac.ComputeHash(Encoding.UTF8.GetBytes(value));
+            }
+        }
+    }
+}
--- np-website/Models/NewMemberModel.cs
+++ np-website/Models/NewMemberModel.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace np_website.Models
 {
-    public class NewMemberModel
+    public class NewMemberModel : Member
     {
-        private const int SaltSize = 32;
 
         [Display(Name = "Email")]
         [Required]
@@ -55,47 +52,6 @@ namespace np_website.Models
                 db.SaveChanges();
 
                 return member.UserId;
-            }
-        }
-
-
-
-
-
-
-        /// <summary>
-        ///     Generates a random hash salt.
-        /// </summary>
-        /// 
-        internal static byte[] GenerateSalt()
-        {
-            using (var rng = new RNGCryptoServiceProvider())
-            {
-                var randomNumber = new byte[SaltSize];
-
-                rng.GetBytes(randomNumber);
-
-                return randomNumber;
-
-            }
-        }
-
-        /// <summary>
-        ///     Hashes the input based on the given salt.
-        /// </summary>
-        /// 
-        /// <returns>
-        ///     Returns the hashed value.
-        /// </returns>
-        /// 
-        /// <param name="value">The value to hash</param>
-        /// <param name="salt">The salt used in the hashing algorithm.</param>
-        /// 
-        internal static byte[] ComputeHMAC_SHA256(string value, byte[] salt)
-        {
-            using (var hmac = new HMACSHA256(salt))
-            {
-                return hmac.ComputeHash(Encoding.UTF8.GetBytes(value));
             }
         }
 
